44394ebd6c2dbdd71b6e29ef8894bcd140c0b63e
Victor Masson <victormasson21@gmail.com>
2019-07-10 12:13:24 +0100

    Adding the test packages

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+test('Function should return an object', function(t) {
+  const actual = someFunction(blahblah);
+  const expected = 'someResult';
+
+  t.pass(actual, expected, 'Some function should give us some result');
   t.end();
 });
